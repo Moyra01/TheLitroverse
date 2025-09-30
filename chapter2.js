7e9eaab0c4e2bce1b43d1613573bb1132b8352fd
Moyra01 <moedhanganyama@gmail.com>
2025-09-30 16:05:50 +0200

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -24,15 +24,15 @@ const chapter2 = [
   `
   <section>
     <p>Then comes the voice. The one that can make your blood pressure spike.</p>
-    <p><strong>“So you don’t even see us? You come back from wherever you were and just stare at us like we’re vomit, neeh?”</strong> says the family witch.</p>
+    <p>“So you don’t even see us? You come back from wherever you were and just stare at us like we’re vomit, neeh?” says the family witch.</p>
     <p>I ignore her and walk deeper into the room, standing by the arch that faces the stairs. I want to see my mother descend the stairs. If I find anything wrong with her, there shall be wailing. My father is already sweating bullets. And when my mother finally appears—no bruises—I know he’s upgraded his abuse tactics. No more fists. Just psychological warfare.</p>
     <p>She smiles at me, but it doesn’t reach her eyes. I walk to meet her halfway. She hugs me. I know she’s crying because she doesn’t want to let go.</p>
     <p>“What’s wrong, Mama?” I ask. She’s cried over this man before, but this time? This is deep. This is soul-level pain.</p>
-    <p><strong>“Your mother is just being a baby. People take second wives all the time. There’s nothing special about her,”</strong> says my father’s advocate, the witch.</p>
+    <p>“Your mother is just being a baby. People take second wives all the time. There’s nothing special about her,” says my father’s advocate, the witch.</p>
     <p>The silence that follows is so loud, everyone is waiting for me to lose it.</p>
-    <p><strong>“Why are you staying, Mama? Do you even have a breaking point? What will it take for you to leave this man?”</strong></p>
+    <p>“Why are you staying, Mama? Do you even have a breaking point? What will it take for you to leave this man?”</p>
     <p>Cue the laughter. The kind that’s not funny. The provoking kind.</p>
-    <p><strong>“Ha ha ha! Can you see how old she looks? How worn out she is? Even with all that mud she puts on her face, she couldn’t attract an old man on his deathbed. Don’t be funny, Austin. Who do you think would want her now? After all, the only reason she got my brother back in the day is because she’s a witch that—”</strong></p>
+    <p>“Ha ha ha! Can you see how old she looks? How worn out she is? Even with all that mud she puts on her face, she couldn’t attract an old man on his deathbed. Don’t be funny, Austin. Who do you think would want her now? After all, the only reason she got my brother back in the day is because she’s a witch that—”</p>
     <p>She doesn’t finish her sentence because she’s now choking. On her own words. And my hand.</p>
     <p>I’m in my zone. She’s kicking, scratching, trying to slap my hand away. But she shouldn’t be turning blue this fast. It’s not even my whole hand around her neck—just a few fingers and her fat neck doing the rest of the work.</p>
   </section>
@@ -41,7 +41,7 @@ const chapter2 = [
   // Page 6
   `
   <section>
-    <p>Then my mother begins to recite our clan praises. Calm. Regal. Like she is trying to calm a storm.</p>
+    <p>Then my mother begins to recite our clan praises. Like she is trying to calm a storm.</p>
     <blockquote>
       <p><strong>“Oh mighty Chitova, leg of strength and grace,<br>
       You carry the weight, yet hold a peaceful face.<br>
@@ -56,12 +56,12 @@ const chapter2 = [
       Let not wrath cloud the name you bear,<br>
       For strength is in peace and the love you share.”</strong></p>
     </blockquote>
-    <p>I drop her to the floor just as my mother finishes. My aunt gasps for air like a fish out of water. I turn to the room.</p>
-    <p><strong>“Does anyone have anything to say?”</strong> I ask, finally addressing the crowd.</p>
-    <p><strong>Crickets.</strong> If you are not part of this family, I expect you to be gone before I get to 5. Gasps. I don’t even get to 2 before the room is clear of the gossipmongers that were here. <strong>Cowards.</strong></p>
+    <p>I drop her to the floor just as my mother finishes. She gasps for air. I turn to the room.</p>
+    <p>“Does anyone have anything to say?” I ask, finally addressing the crowd.</p>
+    <p>If you are not part of this family, I expect you to be gone before I get to 5. Gasps. I don’t even get to 2 before the room is clear of the gossipmongers that were here. <strong>Cowards.</strong></p>
     <p>I look across the room and my eyes settle on Her. Face of a sixteen-year-old, body of a mature African woman, and smooth skin. I croak my head as if trying to understand her, and she stares back at me.</p>
-    <p><strong>‘That is your second mum son,’</strong> the devil’s son says. I mask my shock and chuckle.</p>
-    <p><strong>‘Well, looks like it's going to be fun being back home.’</strong> Gasps, yeah, people are shocked, well, me too. I guess I’m moving back in. <strong>Home sweet home!</strong></p>
+    <p>‘That is your second mum son,’the devil’s son says. I mask my shock and chuckle.</p>
+    <p>‘Well, looks like it's going to be fun being back home.’ Gasps, yeah, the family is shocked, well, me too. I guess I’m moving back in. Home sweet home!</p>
   </section>
   `
 ];
